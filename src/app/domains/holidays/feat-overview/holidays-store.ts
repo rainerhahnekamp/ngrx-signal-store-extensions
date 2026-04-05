27bae2800e73aa1b34b89d3d6c5fbeddc1b15c06
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2026-04-05 02:39:38 +0200

    feat: add holidayStore

---
+++ src/app/domains/holidays/feat-overview/holidays-store.ts
@@ -0,0 +1,48 @@
+import { inject, signal } from '@angular/core';
+import { Holiday } from '../model/holiday';
+import { MessageService } from '../../../shared/ui-messaging/message/message.service';
+
+abstract class BaseStore<T = Record<string, unknown>> {
+  getState(): T {
+    return {} as T;
+  }
+
+  patchState(value: Partial<T>) {}
+}
+
+abstract class LocalStorageStore extends BaseStore {
+  private readonly messageService = inject(MessageService);
+
+  constructor(private storageKey: string) {
+    super();
+  }
+
+  syncToStorage() {
+    const state = this.getState();
+    localStorage.setItem(this.storageKey, JSON.stringify(state));
+    this.messageService.info('Holidays saved for offline usage');
+  }
+
+  loadFromStorage() {
+    const data = localStorage.getItem(this.storageKey);
+    if (!data) {
+      return;
+    }
+
+    const state = JSON.parse(data);
+    this.patchState(state);
+  }
+}
+
+abstract class FinalStore extends LocalStorageStore {
+  private readonly _favouriteIds = signal([] as number[]);
+  public readonly favouriteIds = this._favouriteIds.asReadonly();
+}
+
+export class HolidaysStore extends FinalStore {
+  constructor() {
+    super('holidays');
+  }
+  private readonly _holidays = signal([] as Holiday[]);
+  public readonly holidays = this._holidays.asReadonly();
+}
